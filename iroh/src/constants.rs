8fb987b2b2d4845f4be1b232f695c6d326ace0fd
b5 <sparkle_pony_2000@qri.io>
2022-10-04 14:44:16 -0400

    refine commands based on meeting notes

--- iroh/src/constants.rs
+++ iroh/src/constants.rs
@@ -64,6 +64,43 @@ pub static ID_AFTER_TEXT: &str = "";
 pub static P2P_AFTER_TEXT: &str = "";
 pub static P2P_CONNECT_AFTER_TEXT: &str = "";
 pub static P2P_DISCONNECT_AFTER_TEXT: &str = "";
-pub static START_AFTER_TEXT: &str = "";
+pub static START_AFTER_TEXT: &str = "
+The daemon will start listening on ports on the network, which are
+documented in (and can be modified through) 'ipfs config Addresses'.
+For example, to change the 'Gateway' port:
+
+  ipfs config Addresses.Gateway /ip4/127.0.0.1/tcp/8082
+
+The API address can be changed the same way:
+
+  ipfs config Addresses.API /ip4/127.0.0.1/tcp/5002
+
+Make sure to restart the daemon after changing addresses.
+
+By default, the gateway is only accessible locally. To expose it to
+other computers in the network, use 0.0.0.0 as the ip address:
+
+  ipfs config Addresses.Gateway /ip4/0.0.0.0/tcp/8080
+
+Be careful if you expose the API. It is a security risk, as anyone could
+control your node remotely. If you need to control the node remotely,
+make sure to protect the port as you would other services or database
+(firewall, authenticated proxy, etc).
+
+Shutdown
+
+To shut down the daemon, send a SIGINT signal to it (e.g. by pressing 'Ctrl-C')
+or send a SIGTERM signal to it (e.g. with 'kill'). It may take a while for the
+daemon to shutdown gracefully, but it can be killed forcibly by sending a
+second signal.
+
+IPFS_PATH environment variable
+
+ipfs uses a repository in the local file system. By default, the repo is
+located at ~/.ipfs. To change the repo location, set the $IPFS_PATH
+environment variable:
+
+  export IPFS_PATH=/path/to/ipfsrepo
+";
 pub static STATUS_AFTER_TEXT: &str = "";
 pub static VERSION_AFTER_TEXT: &str = "";
--- iroh/src/main.rs
+++ iroh/src/main.rs
@@ -5,7 +5,7 @@ use iroh::constants;
 
 fn cli() -> Command<'static> {
     Command::new("iroh")
-        .about("A next generation IPFS implementation")
+        .about("A next generation IPFS implementation: https://iroh.computer")
         .subcommand_required(true)
         .arg_required_else_help(true)
         .allow_external_subcommands(true)
@@ -16,18 +16,16 @@ fn cli() -> Command<'static> {
                 .arg(arg!(<PATH> "The path to a file to be added to IPFS"))
                 .args(vec![
                     arg!(-r --recursive "Add directory paths recursively. Default: true"),
-                        // .default_value(true),
                     arg!( --"stdin-name" ... "Assign a name if the file source is stdin")
                         .required(false),
                     arg!(-H --hidden "Include files that are hidden. Only takes effect on recursive add.")
                         .required(false),
-                    arg!(-p --progress "Stream progress data. Defualt: true"),
-                        // .default_value(true),
+                    arg!(-p --progress "Stream progress data. Default: true"),
                     arg!(-n --"only-hash" "Only chunk and hash. Do not write to disk.")
                         .required(false),
                     arg!(-w --"wrap-with-directory" "Wrap files with a directory object. Default: true"),
-                        // .default_value(true),
-                    arg!(--pin "Pin this object when adding. Default: true."),
+                    // we aren't going to do anything about pinning this release
+                    // arg!(--pin "Pin this object when adding. Default: true."),
                         // .default_value(true)
                 ])
                 .arg_required_else_help(true),
@@ -37,17 +35,10 @@ fn cli() -> Command<'static> {
                 .about("get things")
                 .arg(arg!(<CID> ... "CID to get"))
                 .args(vec![
-                    arg!(-o --output [OUTPUT] "The path where the output should be stored.").value_parser(clap::value_parser!(PathBuf)),
+                    arg!(-o --output [OUTPUT] "The path where the output should be stored.").value_parser(clap::value_parser!(PathBuf))
+                        .required(false),
                 ])
                 .after_help(constants::GET_AFTER_TEXT)
-                .arg_required_else_help(true),
-        )
-        .subcommand(
-            Command::new("id")
-                .about("identify iroh")
-                .arg(arg!(--"peerid-base" "Encoding used for peer IDs: Can either be a multibase encoded CID or a base58btc encoded multihash. Takes {b58mh|base36|k|base32|b...}. Default: b58mh.")
-                    .default_value("b58mh"))
-                .after_help(constants::ID_AFTER_TEXT)
                 .arg_required_else_help(true),
         )
         .subcommand(
@@ -57,33 +48,29 @@ fn cli() -> Command<'static> {
                 .after_help(constants::P2P_AFTER_TEXT)
                 .arg_required_else_help(true)
                 .subcommand(
+                    Command::new("id")
+                    .about("identify iroh")
+                    .arg(arg!(--"peerid-base" "Encoding used for peer IDs: Can either be a multibase encoded CID or a base58btc encoded multihash. Takes {b58mh|base36|k|base32|b...}. Default: b58mh.")
+                        .default_value("b58mh"))
+                    .after_help(constants::ID_AFTER_TEXT)
+                )
+                .subcommand(
                     Command::new("connect")
                         .about("connect to a peer")
                         .arg(arg!(<ADDRESS> ... "address of a peer to connect to"))
                         .after_help(constants::P2P_CONNECT_AFTER_TEXT)
                         .arg_required_else_help(true),
                 )
-                .subcommand(
-                    Command::new("disconnect")
-                        .about("disconnect from a peer")
-                        .arg(arg!(<ADDRESS> ... "address of a peer to disconnect from"))
-                        .after_help(constants::P2P_DISCONNECT_AFTER_TEXT)
-                        .arg_required_else_help(true),
-                )
         )
         .subcommand(
-            Command::new("connect")
-                .about("start a long running IPFS process")   
+            Command::new("start")
+                .about("start a long running IPFS process")
                 .after_help(constants::START_AFTER_TEXT)
         )
         .subcommand(
             Command::new("status")
                 .about("report current status of iroh")
                 .after_help(constants::STATUS_AFTER_TEXT)
-        )
-        .subcommand(
-            Command::new("version")
-                .about("print version information")
         )
 }
 
